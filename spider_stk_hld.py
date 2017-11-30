d295b52026ad6066bdb3558586140a96518a07ab
jeepx5 <jimway2002@hotmail.com>
2017-11-30 14:27:16 +0800

    Add files via upload

---
+++ spider_stk_hld.py
@@ -0,0 +1,54 @@
+#-*_coding:utf8-*-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
+import json
+
+
+class stk_hld():
+    def __init__(self):
+        pass
+
+    def stk_hld_scrapy(self, url, code):
+        dcap = dict(DesiredCapabilities.PHANTOMJS)  # 设置useragent
+        dcap['phantomjs.page.settings.userAgent'] = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 ')  # 根据需要设置具体的浏览器信息
+        driver = webdriver.PhantomJS('D:\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe', desired_capabilities=dcap)
+        driver.get(url)
+        xpth_table = '/html/body/div[@id="wrap"]/div[@id="main"]/div[@id="center"]/div[@class="centerImgBlk"]/div[@id="con02-2"]/table[@id="Table1"]/tbody'
+        products = driver.find_element_by_xpath(xpth_table).text
+        if len(products) >0:
+            print('data obtained. Ready to parse')
+        else:
+            print('no data obtained, please check the internet connection')
+        stk_hld = {'key': code,
+                   'name': [],
+                   'num': [],
+                   'ratio': [],
+                   'type': []}
+        m = 1
+        i = 0
+        for item in products.split('\n'):
+
+            case = str(m)
+            #print(case)
+            if item == case:
+                print(str(m / 10 * 100) + '%')
+                m += 1
+                # print(products.split('\n')[i+1])
+                stk_hld['name'].append(products.split('\n')[i + 1])
+                stk_hld['num'].append(products.split('\n')[i + 2])
+                stk_hld['ratio'].append(products.split('\n')[i + 3])
+                stk_hld['type'].append(products.split('\n')[i + 4])
+                if m==11:
+                    break
+            i += 1
+
+
+            # print(item)
+
+        print('\n========data extracted==========')
+        return stk_hld
+
+
+
+
+
