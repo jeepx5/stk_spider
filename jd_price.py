e5b8d9cc757f6f59fb6a0991a4b2d295be7ab095
jeepx5 <jimway2002@hotmail.com>
2017-12-06 14:01:49 +0800

    add release date and stk list
    grab the stk list from tushare

--- jd_price.py
+++ jd_price.py
@@ -8,21 +8,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from spider_stk_hld import stk_hld
+import tushare as ts
+import pandas as pd
+import random
 
+
+df=ts.get_stock_basics()
+df.to_csv('.\stk_basic.csv')#, encoding='utf-8')
+#df=pd.read_csv('.\stk_basic.csv')
+clst=df.index.tolist()
+#clst=df['code'].tolist()
+print(clst)
+
+
+time.sleep(5)
 urlbase = 'http://money.finance.sina.com.cn/corp/go.php/vCI_StockHolder/stockid/'
 urltail = '/displaytype/30.phtml'
 
-codelst=['000528', '600006', '002426']
-
+#codelst=['000528', '600006', '002426']
+codelst=clst
 stkhld = {}
 
 
 
 for code in codelst:
-    url = urlbase + code + urltail
+    url = urlbase + str(code) + urltail
     print(url)
     spider = stk_hld()
     stkhld[code] = spider.stk_hld_scrapy(url, code)
+    rnd=random.gauss(25, 13)
+    time.sleep(rnd)
+
+
+
 
 print(stkhld)
 print(stkhld.keys())
+stk_df=pd.DataFrame.from_dict(stkhld, orient='index', dtype=None)
+stk_df.to_csv('.\stkhold.csv')
